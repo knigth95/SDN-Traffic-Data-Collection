905834999c9a77770c61a831134881250c9e57bd
knigth95 <knight20030209@163.com>
2023-12-25 16:00:47 +0800

    SDN相关

--- app.py
+++ app.py
@@ -26,63 +26,90 @@ signal.signal(signal.SIGTERM, signal_handler)
 
 app = Flask(__name__, template_folder='./tempates')
 
-
-# 用于存储子进程的字典
-subprocesses = {}
-
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/start_ryu', methods=['POST'])
-def start_ryu():
-    proc = subprocess.Popen(['gnome-terminal', '--', 'ryu-manager', 'controller.py'])
-    subprocesses['ryu'] = proc
-    return jsonify(success=True)
+    return render_template('index1.html')
 
 @app.route('/start_mininet', methods=['POST'])
-def start_mininet():
-    # 下面的 'sudo' 密码方式不是一个安全的做法，应仅作为示例。请寻找更合适的解决方案。
-    proc = subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', 'echo 123456 | sudo -S python topology.py'])
-    subprocesses['mininet'] = proc
-    return jsonify(success=True)
-
-@app.route('/show_topo', methods=['GET'])
-def show_topo():
-    # 实际的拓扑数据应该由其他逻辑产生或从其他地方读取
-    topo_data = '<svg>Your SVG topology diagram here.</svg>'
-    return jsonify(topology=topo_data)
-
-@app.route('/start_script/<int:host_id>', methods=['POST'])
-def start_script(host_id):
-    script_name = 'script{}.sh'.format(host_id)
-    proc = subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', 'xterm -hold -e bash ./scripts/{}'.format(script_name)])
-    subprocesses['h{}}'.format(host_id)] = proc
-    return jsonify(success=True)
-
-@app.route('/stop_script/<int:host_id>', methods=['POST'])
-def stop_script(host_id):
-    proc = subprocesses.get('h{}'.format(host_id))
-    if proc:
-        proc.send_signal(signal.SIGINT) # or signal.SIGKILL
-        return jsonify(success=True)
-    return jsonify(success=False)
-
-@app.route('/file_content/<int:file_id>', methods=['GET'])
-def file_content(file_id):
-    file_name = 'h{}file.txt'.format(file_id)
+def mininet_start():
     try:
-        with open(file_name, 'r') as f:
-            file_content = f.read()
-        return jsonify({'file_content':file_content}),200
-        #return send_file(file_name, as_attachment=False)
-    except FileNotFoundError:
-        return jsonify(error='File not found'), 404
-
-if __name__ == '__main__':
-    try:
-        app.run(host='0.0.0.0', port=2004, debug=True,use_reloader=True)
+        password = 'bsj'  # 注意这是一个安全风险，实际使用中请使用更安全的方法
+        
+        # 打开一个新的终端窗口来运行 ryu-manager
+        if sys.platform == "win32":
+            topology_command = 'start cmd /k python3 topology.py'
+        elif sys.platform == "darwin":
+            topology_command = "osascript -e 'tell app \"Terminal\" to do script \"python3 topology.py\"'"
+        elif sys.platform.startswith('linux'):
+            topology_command = 'echo "{}" | sudo -S python3 topology.py'.format(password)
+        else:
+            raise EnvironmentError("Unsupported platform")
+        
+        
+        subprocess.Popen(topology_command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        return jsonify({'message': 'mininet is starting...'}), 200
     except Exception as e:
-        print('Error occurred: {}'.format(e))
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/start_ryu', methods=['POST'])
+def ryu_start():
+    try:
+        password = 'bsj'  # 注意这是一个安全风险，实际使用中请使用更安全的方法
+        
+        # 打开一个新的终端窗口来运行 ryu-manager
+        if sys.platform == "win32":
+            ryu_command = 'start cmd /k ryu-manager controller.py'
+        elif sys.platform == "darwin":
+            ryu_command = "osascript -e 'tell app \"Terminal\" to do script \"ryu-manager controller.py\"'"
+        elif sys.platform.startswith('linux'):
+            ryu_command = "gnome-terminal -- bash -c 'ryu-manager controller.py'"
+        else:
+            raise EnvironmentError("Unsupported platform")
+        
+        subprocess.Popen(ryu_command, shell=True)
+        return jsonify({'message': 'Network is starting...'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/open_xterm/<host_id>', methods=['POST'])
+def open_xterm(host_id):
+    try:
+        script_name = 'script{}.sh'.format(host_id)
+        # 修改这里，使xterm打开后立即执行指定的脚本
+        subprocess.Popen('xterm -title "Host {}" -e "bash ./{}"'.format(host_id, script_name), shell=True)
+        
+        return jsonify({'message': 'Xterm for Host {} with script {} is opened.'.format(host_id, script_name)}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/script/<host_id>', methods=['POST'])
+def script(host_id):
+    action = request.form['action']  # "start" or "stop"
+    script_name = 'script{}.sh'.format(host_id)
+    try:
+        if action == 'start':
+            subprocess.Popen(['/bin/bash', script_name], cwd=os.getcwd())
+        elif action == 'stop':
+            subprocess.Popen(['/bin/bash', script_name, 'stop'], cwd=os.getcwd())
+        return jsonify({'message': 'Action {} for script {} executed.'.format(action, script_name)}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/show_file/<file_id>', methods=['GET'])
+def show_file(file_id):
+    filename = 'h{}file.txt'.format(file_id)
+    try:
+        with open(filename, 'r') as f:
+            file_content = f.read()
+        return jsonify({'file_content': file_content}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == "__main__":
+    try:
+        app.run(host='0.0.0.0', port=2003, debug=True, use_reloader=True)
+    except Exception as e:
+        print("An exception occurred: {}".format(e))
     finally:
         release_resources()
